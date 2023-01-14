4aacf61441eaa0af32139ff4354cbae272b26ff3
ashwabu <abdulazizredi90@gmail.com>
2023-01-14 23:08:27 +0300

    Footer started

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import css from './Footer.module.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={css.Footer}>
+      <hr />
+      <h1>the date may be one or two days wrong</h1>
+      <p>ibn shihabudin &copy;All rights riserved 2023GC(1444H)</p>
+
+    </div>
   )
 }
 
